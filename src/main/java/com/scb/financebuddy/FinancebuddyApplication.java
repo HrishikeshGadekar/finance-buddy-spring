8cc5f5f4c7e0f8c06f84dc61e0ce95ed0f713927
Hrishikesh Gadekar <hrishikesh.gadekar89@gmail.com>
2023-05-19 22:52:15 +0530

    Initial commit

---
+++ src/main/java/com/scb/financebuddy/FinancebuddyApplication.java
@@ -0,0 +1,13 @@
+package com.scb.financebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancebuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinancebuddyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/scb/financebuddy/FinancebuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scb.financebuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FinancebuddyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
